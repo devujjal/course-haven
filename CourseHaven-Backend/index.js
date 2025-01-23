df4916a53a60a1d9afc636f8b60618bcce34bb7b
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-24 01:55:41 +0600

    make endpoint for individual category

--- CourseHaven-Backend/index.js
+++ CourseHaven-Backend/index.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PROT || 5000;
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iam7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const database = client.db('courseHaven');
+        const courses = database.collection('courses');
+
+        app.get('/courses/:category', async (req, res) => {
+            try {
+                const category = req.params.category;
+                console.log(category)
+            } catch (error) {
+                res.status(500).send({ message: 'Internal Server Error' });
+            }
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', async (req, res) => {
     res.send('Course Haven is Running!😁')
