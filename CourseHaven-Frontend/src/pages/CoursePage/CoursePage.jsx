c3b5f8af62016f8cb15225f7e093eea3624b59fb
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-25 17:00:04 +0600

    pagination done

--- CourseHaven-Frontend/src/pages/CoursePage/CoursePage.jsx
+++ CourseHaven-Frontend/src/pages/CoursePage/CoursePage.jsx
@@ -69,6 +69,23 @@ const CoursePage = () => {
     }
 
 
+    const handleNextBtn = () => {
+        if (currentPage === pages.length - 1) return;
+
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
+
+    const handlePreviewBtn = () => {
+        if (currentPage === 0) return;
+
+        if (pages.length > 0) {
+            setCurrentPage(currentPage - 1)
+        }
+
+    }
+
     // console.log(numberOfCourses)
     // console.log(perPageItems)
     // console.log(totalPages)
@@ -103,12 +120,13 @@ const CoursePage = () => {
                                     ))
                             }
                         </div>
-
+                        {/* bg-gray-100 */}
                         <div className="mt-14">
                             <ul className="flex space-x-5 justify-center font-roboto">
-                                <li 
-
-                                className="flex items-center justify-center shrink-0 bg-gray-100 w-10 h-10 rounded-full">
+                                <li
+                                    onClick={handlePreviewBtn}
+                                    aria-disabled={currentPage === 0}
+                                    className={`${currentPage === 0 ? 'cursor-not-allowed bg-gray-100' : 'hover:bg-gray-50 cursor-pointer'} flex items-center justify-center shrink-0  border-2 w-10 h-10 rounded-full`}>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-3 fill-gray-300" viewBox="0 0 55.753 55.753">
                                         <path
                                             d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
@@ -131,7 +149,10 @@ const CoursePage = () => {
 
 
 
-                                <li className="flex items-center justify-center shrink-0 hover:bg-gray-50 border-2 cursor-pointer w-10 h-10 rounded-full">
+                                <li
+                                    onClick={handleNextBtn}
+                                    aria-disabled={currentPage === pages.length - 1}
+                                    className={`${currentPage === pages.length - 1 ? 'cursor-not-allowed bg-gray-100' : 'hover:bg-gray-50  cursor-pointer'} flex items-center justify-center shrink-0 border-2 w-10 h-10 rounded-full`}>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-3 fill-gray-400 rotate-180" viewBox="0 0 55.753 55.753">
                                         <path
                                             d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
