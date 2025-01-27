988879164348ef7809b8914d51edbfae28617c7d
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-27 16:08:00 +0600

    add adminSideBar

---
+++ CourseHaven-Frontend/src/components/Dashboard/AdminSideBar/AdminSideBar.jsx
@@ -0,0 +1,24 @@
+import NavLinkMenu from "../NavLink/NavLink";
+import { MdDashboard } from "react-icons/md";
+import { CiShoppingBasket } from "react-icons/ci";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { FaUserFriends } from "react-icons/fa";
+import { PiNotebookFill } from "react-icons/pi";
+import { FaEdit } from "react-icons/fa";
+import { LiaSignOutAltSolid } from "react-icons/lia";
+
+const AdminSideBar = () => {
+    return (
+        <>
+            <NavLinkMenu title={'Dashboard'} icon={MdDashboard} />
+            <NavLinkMenu title={'My Courses'} icon={CiShoppingBasket} />
+            <NavLinkMenu title={'Earnings'} icon={FaArrowTrendUp} />
+            <NavLinkMenu title={'Students'} icon={FaUserFriends} />
+            <NavLinkMenu title={'Orders'} icon={PiNotebookFill} />
+            <NavLinkMenu title={'Edit Profile'} icon={FaEdit} />
+            <NavLinkMenu title={'Sign Out'} icon={LiaSignOutAltSolid} />
+        </>
+    );
+};
+
+export default AdminSideBar;
