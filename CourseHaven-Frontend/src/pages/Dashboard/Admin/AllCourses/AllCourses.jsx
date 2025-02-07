a88c0bc4a11ca94f482444e540a0299631e7f69e
Developer Ujjal <ujjalchandra213@gmail.com>
2025-02-07 23:13:23 +0600

    add search and sorting menu

--- CourseHaven-Frontend/src/pages/Dashboard/Admin/AllCourses/AllCourses.jsx
+++ CourseHaven-Frontend/src/pages/Dashboard/Admin/AllCourses/AllCourses.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import { PiListDashesFill } from "react-icons/pi";
 import { IoCloseSharp } from "react-icons/io5";
 import { FaEdit } from "react-icons/fa";
+import SortMenu from "./SortMenu";
 
 
 const AllCourses = () => {
@@ -27,7 +28,42 @@ const AllCourses = () => {
                         <h2 className="text-[#24292d] text-3xl font-heebo font-bold">My Courses List
                         </h2>
                     </div>
-                    <div className="mt-6 overflow-x-auto px-2 md:px-4"> {/* Ensure this div has overflow-x-auto */}
+
+
+                    <div>
+                        <div className="flex flex-col xl:flex-row justify-between items-center mt-6 px-2 md:px-4 gap-5">
+                            <form className="relative w-full xl:w-[60%]">
+                                <input
+                                    type="text"
+                                    placeholder="Search"
+                                    className="w-full pr-10 px-4 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#066ac9] transition-all duration-300"
+                                />
+
+                                <button
+                                    type="submit"
+                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 cursor-pointer">
+                                    <svg
+                                        className=""
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                        ></path>
+                                    </svg></button>
+                            </form>
+
+                            <SortMenu />
+                        </div>
+                    </div>
+
+
+                    <div className="mt-8 overflow-x-auto px-2 md:px-4"> {/* Ensure this div has overflow-x-auto */}
                         <div className="min-w-[800px]"> {/* Set a minimum width to force overflow */}
                             <table className="w-full text-left border-collapse">
                                 <thead className="bg-gray-800 text-white text-base font-bold font-roboto rounded-t-lg">
@@ -70,7 +106,7 @@ const AllCourses = () => {
                                                 {payment?.enrolled}
                                             </td>
                                             <td className="p-4 font-roboto">
-                                                <span className={`px-2 py-1 rounded-full text-xs font-semibold bg-green-100 text-green-600`}>{'Paid'}</span> 
+                                                <span className={`px-2 py-1 rounded-full text-xs font-semibold bg-green-100 text-green-600`}>{'Paid'}</span>
                                             </td>
                                             <td className="p-4 font-roboto text-[15px] font-normal">
                                                 {payment?.price}
---
+++ CourseHaven-Frontend/src/pages/Dashboard/Admin/AllCourses/SortMenu.jsx
@@ -0,0 +1,38 @@
+import { Field, Select } from '@headlessui/react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+
+
+const SortMenu = () => {
+
+
+    return (
+        <div className=" w-full md:w-[20%]">
+            <Field>
+                <div className="relative font-roboto text-[#404448]">
+                    <Select
+                        // onChange={(e) => handleChange(e.target.value)}
+                        className={clsx(
+                            'block w-full appearance-none rounded-lg bg-white/5 py-2 px-4 text-sm/6 border text-[#404448]',
+                            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
+                            // Make the text of each option black on Windows
+                            '*:text-[#404448]'
+                        )}
+                    >
+                        <option value="">Sort by</option>
+                        <option value="newest">Newest</option>
+                        <option value="oldest">Oldest</option>
+                       
+                    </Select>
+
+                </div>
+            </Field>
+        </div>
+    )
+}
+
+SortMenu.propTypes = {
+    
+}
+
+export default SortMenu;
